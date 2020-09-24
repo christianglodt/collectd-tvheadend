024127b27495209c87a178e7904f0057f5a39db6
Christian Glodt <chris@mind.lu>
2020-09-24 14:17:21 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# collectd-tvheadend
+# Collectd plugin for Tvheadend
+
+`collectd-tvheadend` is a simple Python3-based [collectd](https://collectd.org)
+plugin for gathering statistics from [Tvheadend](https://tvheadend.org).
+
+It gathers input-related statistics as documented [here](https://github.com/dave-p/TVH-API-docs/wiki/status#statusinputs).
+
+## Installation
+
+Clone this repository into a suitable directory.
+
+Create the configuration file `/etc/collectd/collectd.conf.d/tvheadend.conf`
+as follows and adapt it to your specific situation:
+```
+LoadPlugin python
+&lt;Plugin python&gt;
+    ModulePath "/path/to/collectd-tvheadend"
+    LogTraces true
+    Interactive false
+    Import "collectd_tvheadend"
+    &lt;Module collectd_tvheadend&gt;
+        # Base url of Tvheadend:
+        BaseURL "http://localhost:9981"
+        # Add an HTTP header to the request (eg. for authentication):
+        # Header "Cookie" "foo=bar"
+    &lt;/Module&gt;
+&lt;/Plugin&gt;
+```
---
+++ collectd_tvheadend.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import urllib
+import json
+import collectd
+
+base_url = 'http://localhost:9981'
+headers = {}
+
+def config(conf):
+    global base_url
+    global headers
+
+    for node in conf.children:
+        if node.key == 'BaseURL':
+            base_url = node.values[0]
+        if node.key == 'Header':
+            headers[node.values[0]] = node.values[1]
+
+def read(data=None):
+    global base_url
+    global headers
+
+    req = urllib.request.Request(f'{base_url}/api/status/inputs', headers=headers)
+    data = json.loads(urllib.request.urlopen(req).read().decode('utf-8'))
+    for entry in data['entries']:
+        input = entry['input']
+        if entry['signal_scale'] == 1:
+            signal_pct = entry['signal'] / 65535.0
+            collectd.Values(type='gauge').dispatch(values=[signal_pct], plugin='tvheadend_input', plugin_instance=input, type_instance='signal_pct')
+
+        if entry['signal_scale'] == 2:
+            signal_db = entry['signal'] / 1000.0
+            collectd.Values(type='gauge').dispatch(values=[signal_db],plugin='tvheadend_input', plugin_instance=input, type_instance='signal_db')
+
+        if entry['snr_scale'] == 1:
+            snr_pct = entry['snr'] / 65535.0
+            collectd.Values(type='gauge').dispatch(values=[snr_pct], plugin='tvheadend_input', plugin_instance=input, type_instance='snr_pct')
+
+        if entry['snr_scale'] == 2:
+            snr_db = entry['snr'] / 1000.0
+            collectd.Values(type='gauge').dispatch(values=[snr_db], plugin='tvheadend_input', plugin_instance=input, type_instance='snr_db')
+
+        for var_name in ('ber', 'bps', 'cc', 'ec_bit', 'ec_block', 'subs', 'tc_bit', 'tc_block', 'te', 'unc', 'weight'):
+            v = entry.get(var_name, None)
+            if v is not None:
+                collectd.Values(type='gauge').dispatch(values=[v], plugin='tvheadend_input', plugin_instance=input, type_instance=var_name)
+
+collectd.register_config(config)
+collectd.register_read(read)
